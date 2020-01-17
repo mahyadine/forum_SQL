cfb9bac6325bae5b229131aa8f78bff75bd0ab73
mahyadine <yassimahyadine@gmail.com>
2020-01-17 10:52:14 +0100

    create connect Account

---
+++ connect/connectAccount.py
@@ -0,0 +1,11 @@
+from connection import *
+
+class Account():
+    def __init__(self):
+        self.choice = connection()
+        self.peusdo = None
+        self.password = None
+        
+    def connect_user(self):
+        self.peusdo = input("Quel es votre pseudo ? ")
+        self.password = (input("Votre mot de passe : "))
